536a689eb78e95b1c64693879dc619cc18003077
Vinicius Branco <viniblima2016@gmail.com>
2024-07-19 18:33:13 -0300

    passing database infos to env

--- database/database.go
+++ database/database.go
@@ -16,10 +16,13 @@ var Db *gorm.DB
 func ConnectDb() {
 
 	dsn := fmt.Sprintf(
-		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Shanghai",
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
