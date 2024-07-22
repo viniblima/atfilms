f9ff403680250ad3387cc9ae7e7c0bc87e563e9b
Vinicius Branco <viniblima2016@gmail.com>
2024-07-22 14:34:17 -0300

    use body index in creation of customer

--- controllers/customer.controller.go
+++ controllers/customer.controller.go
@@ -46,6 +46,7 @@ func (controller customerController) CreateCustomer(c *fiber.Ctx) error {
 	customer := models.Customer{
 		Name:       body.Name,
 		ShowInHome: &body.ShowInHome,
+		Index:      body.Index,
 	}
 	newCustomer, err := controller.customerRepo.CreateCustomer(&customer)
 
