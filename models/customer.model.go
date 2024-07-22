2d7c98c151c3163b5c7e9ab4a135fb877024696e
Vinicius Branco <viniblima2016@gmail.com>
2024-07-22 14:17:00 -0300

    remove min=1 on struct customer

--- models/customer.model.go
+++ models/customer.model.go
@@ -11,7 +11,7 @@ type Customer struct {
 	Name       string `json:"Name" validate:"required,min=3,max=32"`
 	Logo       Photo  `gorm:"foreignKey:ReferenceID"`
 	ShowInHome bool   `json:"ShowInHome"`
-	Index      int    `json:"Index" validate:"required,min=1"`
+	Index      int    `json:"Index" validate:"required`
 }
 
 func (m *Customer) BeforeCreate(db *gorm.DB) (err error) {
